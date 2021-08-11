f7d7de6fc3d545f37a75c79bcafe2da7fb6f0a6e
Monika Kundu <mkundu993@gmail.com>
2021-08-11 13:12:47 +0600

    dashboard updated with dynamic data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
+use App\Models\Transaction;
+use App\Models\User;
+
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -9,6 +13,14 @@ class DashboardController extends Controller
     public function index()
     {
         $data['title']='Dashboard';
+        $data['logo']= Setting::where('type','logo')->first();
+        $data['total_employee']= User::where('status','Active')->count();
+        $data['total_transaction']= Transaction::count();
+        $data['total_income']= Transaction::where('type','Income')->sum('amount');
+        $data['total_expense']= Transaction::where('type','Expense')->sum('amount');
+        $data['last_3_income']= Transaction::with(['relTransactionHead','relUser'])->where('type','Income')->orderBy('id','DESC')->limit(3)->get();
+        $data['last_3_expense']= Transaction::with(['relTransactionHead','relUser'])->where('type','Expense')->orderBy('id','DESC')->limit(3)->get();
+        $data['users']= User::with('relDesignation')->orderBy('id','DESC')->limit(5)->get();
         return view('admin/dashboard',$data);
     }
 }
